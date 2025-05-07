f6e58e200c561e34ffd16373ad34871342944fe1
Vasudev Kishan <vasudevkishan@gmail.com>
2025-05-07 09:52:12 +0530

    Add sidebar and blank pages for each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import "./App.css";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import DashboardPage from "./pages/DashboardPage";
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import './App.css';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import LandingPage from './pages/LandingPage';
+import Settings from './pages/Settings';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard/:userId" element={<DashboardPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/dashboard/*' element={<LandingPage />} />
+          <Route path='/settings/*' element={<Settings />} />
         </Routes>
       </BrowserRouter>
     </>
