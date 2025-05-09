488141031338d34d863926e62e3e32f08aae15d5
Vasudev Kishan <vasudevkishan@gmail.com>
2025-05-09 23:59:36 +0530

    Add userId in urls

--- src/components/HomepageSidebar.tsx
+++ src/components/HomepageSidebar.tsx
@@ -9,15 +9,19 @@ import {
 } from 'lucide-react';
 import MenuItem from './MenuItem';
 import { useAuth } from '@/hooks/useAuth';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const HomePageSidebar = () => {
   const { logout } = useAuth();
+  const { userId } = useParams<{ userId: string }>();
   return (
     <aside className='w-20 xl:w-1/5 lg:w-1/4  md:w-1/3 bg-accent text-2xl text-sidebar-foreground rounded-tr-2xl'>
       {/* nav bar */}
       <div className='flex mx-auto my-10 w-16 h-16 md:w-full md:h-24 py-3 md:py-6 justify-between items-center bg-foreground md:bg-accent text-primary rounded-full md:rounded-none'>
-        <Link to={'/dashboard'} className='flex mx-auto text-3xl font-bold '>
+        <Link
+          to={`/${userId}/dashboard`}
+          className='flex mx-auto text-3xl font-bold '
+        >
           <span className='w-12 text-accent md:text-primary text-3xl font-bold flex justify-center items-center'>
             <CodeXml />
           </span>
@@ -30,33 +34,37 @@ const HomePageSidebar = () => {
       <MenuItem
         icon={<LayoutDashboard />}
         label='Dashboard'
-        destination='/dashboard'
+        destination={`/${userId}/dashboard`}
       />
       <MenuItem
         icon={<MessageSquareText />}
         label='Posts'
-        destination='/dashboard/posts'
+        destination={`/${userId}/dashboard/posts`}
       />
       <MenuItem
         icon={<Bell />}
         label='Notifications'
-        destination='/dashboard/notifications'
+        destination={`/${userId}/dashboard/notifications`}
       />
       {/* <MenuItem
         icon={<LayoutDashboard />}
         label='Explore'
-        destination='/dashboard/Explore'
+        destination={`/${userId}/dashboard/Explore`}
       /> */}
       <MenuItem
         icon={<User />}
         label='Profile'
-        destination='/dashboard/profile'
+        destination={`/${userId}/dashboard/profile`}
       />
-      <MenuItem icon={<Settings />} label='Settings' destination='/settings' />
+      <MenuItem
+        icon={<Settings />}
+        label='Settings'
+        destination={`/${userId}/settings`}
+      />
       <MenuItem
         icon={<LogOut />}
         label='logout'
-        destination='/login'
+        destination={`/login`}
         onClick={logout}
         replaceDestination={true}
       />
